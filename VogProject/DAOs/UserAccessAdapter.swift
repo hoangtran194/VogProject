ce2831d82f0ae420a8783926c4117a19ec69f8bb
Hoang Tran <hoangtran194@gmail.com>
2020-08-30 01:09:10 -0700

    [iOS] add local access data protocol

---
+++ VogProject/DAOs/UserAccessAdapter.swift
@@ -0,0 +1,13 @@
+//
+//  UserAccessAdapter.swift
+//  VogProject
+//
+//  Created by Hoang Tran on 2020-08-30.
+//  Copyright © 2020 Hoang Tran. All rights reserved.
+//
+
+import UIKit
+
+class UserAccessAdapter: NSObject {
+
+}
